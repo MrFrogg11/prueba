cfe95d8d00b8b9d8eaf04cacc80c0b29f45b6830
MrFrogg11 <miyovirtual2@gmail.com>
2024-01-04 22:55:22 -0300

    prier test

---
+++ test/src/componentes/componente.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Componente() {
+    return (
+        <div>
+            <p>Hola, soy un componente de React</p>
+        </div>
+    )
+}
+
+export default Componente
